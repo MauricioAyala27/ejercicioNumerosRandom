6586aeffecc9beabbd2652c7baecadfa30a18fd4
Mauricio Ayala <mauricioayala1237@gmail.com>
2019-08-02 15:18:32 -0600

    Version 1

---
+++ ejercicioNumerosRandom/src/ejercicioNumerosRandom/main.java
@@ -0,0 +1,34 @@
+package ejercicioNumerosRandom;
+
+import java.util.Random;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		int numram1 = 0;
+		int numram2 = 0;
+		int num1 = 0;
+		int num2 = 0;
+
+		for (int i = 1; i <= 10; i++) {
+
+			/*
+			 * Random r1 = new Random(999); Random r2 = new Random(999);
+			 */
+
+			
+			numram1 = (int) Math.random() * i; 
+			numram2 = (int) Math.random() * i;
+			 
+
+			num1 = numram1 - numram2;
+			num2 = numram1 + numram2;
+
+			System.out.println(numram1 + "+" + numram2 + "=" + num1 + " " + num2);
+
+		}
+
+	}
+
+}
